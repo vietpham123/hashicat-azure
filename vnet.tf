17670159ec1ed8a43408131d9fc22f25eab42dcf
vietpham123 <calijp123@gmail.com>
2021-07-15 19:03:14 +0000

    added vnet module

--- vnet.tf
+++ vnet.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = ""${var.prefix}-workshop""
+  name     = "${var.prefix}-workshop"
   location = "centralus"
 }
 
